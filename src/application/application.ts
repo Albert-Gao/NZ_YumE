da8d42d9c5f8166257c6ea5770795b223546ba7c
Albert Gao <albertgaohy@gmail.com>
2016-08-16 04:01:09 +1200

    add the centralCallback dispatcher to dispatch all the callback to according function, and a function to retrieve the matched callBack function from pages properties, finally add a function to generate text from Yelp API

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,8 +1,10 @@
 import {IApp} from "../emulator/models/dataModels/IApp";
 import {IPage} from "../emulator/models/dataModels/IPage";
 import {page1search} from "./page1search";
+import {page2list} from "./page2list";
 import {IActionService} from "../emulator/models/serviceModels/IActionService";
 import {IFunc} from "../emulator/models/dataModels/IFunction"
+import {IListItem} from "../emulator/models/dataModels/IListItem";
 
 export class application implements IApp {
 
@@ -27,6 +29,7 @@ export class application implements IApp {
 
     startAddingPages() {
         this.pages.push(new page1search());
+        this.pages.push(new page2list());
     }
 
     createPageArray() {
@@ -34,13 +37,39 @@ export class application implements IApp {
     }
 
     appCallback(pageName:string, elementName: string, targetElementInfo?: string) {
-        console.log("I am here at application");
-        console.log(targetElementInfo);
         switch (elementName){
             case "page1button":
                 let func:Function;
-                func = this.getMatchedFunction(pageName, elementName, targetElementInfo);
-                func(this._actionService,targetElementInfo);
+                if (targetElementInfo){
+                    this._actionService.callYelpSearchAPI(targetElementInfo,(json:any)=>{
+                        let list = this.tailorYelpResult(json);
+                        for(let page of this.pages){
+                            if (page.name === 'page2list'){
+                                for(let e of page.rawLayout){
+                                    if (e.type === 'text'&&e.name==='page2text'){
+                                        e.define = list;
+                                    } else if (e.type === 'image') {
+                                        if (typeof json != 'undefined'){
+                                            e.define = <string>json.snippet_image_url;
+                                        }
+                                    }
+                                }
+                                this._actionService.reRenderPage(page);
+                                break;
+                            }
+                        }
+                        func = this.getMatchedFunction(pageName,elementName,targetElementInfo);
+                        func(this._actionService,targetElementInfo);
+                    });
+                } else {
+                    this.getMatchedFunction(pageName,elementName)(this._actionService);
+                }
+
+                break;
+            case "page2button":
+                //OK, Sorry, just want to get rid of it soon...it works,
+                //since I know the exact place.
+                this.pages[1].callback[0].callbackFunction(this._actionService);
                 break;
         }
     }
@@ -64,5 +93,28 @@ export class application implements IApp {
         }
 
         return callback.callbackFunction;
+    }
+
+    tailorYelpResult(json:Object):string{
+        console.log(json);
+        let define:string;
+
+        //Yelp API suck even for v2 version.
+        if (json) {
+            let _item:any = json;
+
+            //Curse you yelp, we need to these shit coding,
+            //to cover your bad non-consistent API design.
+            let title:string = _item.name?_item.name:"";
+            let phone:string = _item.phone?_item.phone:"";
+            let rating:string = _item.rating?_item.rating:"";
+            let category:string = _item.categories?_item.categories[0][0]:"";
+            let comment:string = _item.snippet_text?_item.snippet_text:"";
+
+            define ='<br/><br/>'+'Title: ' + title + '<br/><br/>'+'Phone: ' + phone + '<br/><br/>' +'Rating: ' + rating + '<br/><br/>' +'Category: ' + category + '<br/><br/>' +'Comment: ' + comment + '<br/><br/>';
+        } else {
+            define = '<br/><br/>' + "sorry, Yelp consider your keyword as invalid, please go back and try again."
+        }
+        return define;
     }
 }
