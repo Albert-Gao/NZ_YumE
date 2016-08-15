275916cc6ec02576b8ce9f69d7e5a5b2e41947a8
Thomas Scott <emailthomasscott@gmail.com>
2016-08-15 14:58:53 +1200

    lodash

--- spec/test.spec.ts
+++ spec/test.spec.ts
@@ -1,30 +1,95 @@
-/// <reference path="../_typings/jasmine/jasmine.d.ts" />
 import {IStateService} from "../src/emulator/models/serviceModels/IStateService";
 import {IPage} from "../src/emulator/models/dataModels/IPage";
 import {IApp} from "../src/emulator/models/dataModels/IApp";
 import {IElement} from "../src/emulator/models/dataModels/IElement";
+import {IFunc} from "../src/emulator/models/dataModels/IFunction";
 import {StateService} from "../src/emulator/services/StateService";
 import {IActionService} from "../src/emulator/models/serviceModels/IActionService";
+
+var _ = require('lodash');
 
 it('true is true', () => expect(true).toEqual(true));
 
 describe('Tests for the StateService', () => {
-    it('getStartPageName() should return a string', () => {
-        let checkThisValue: string = "This Page";
-        class MockApp implements IApp {
-            title:string = "";
-		    currentPageName:string = checkThisValue;
-		    startPageName:string = checkThisValue;
-		    pages:Array<IPage>;
-		    injectActionService(as:IActionService) {};
-		    startAddingPages() {};
-		    CentralCallbackFunc(pageName:string, elementID?:string) {
-		    	return "";
-		    }
-        }
+	class MockPage implements IPage {
+		name: string; 
+    	rawLayout:Array<IElement>;
+    	//afterRenderLayout?:JQuery;
+    	callback:Array<IFunc>;
+	}
+	let page1 = new MockPage();
+	page1.name = "page1";
+	let page2 = new MockPage();
+	page2.name = "page2";
 
-        let testStateService: StateService = new StateService(new MockApp());
+	let pageName: string = "page1";
+	let appTitle: string = "Title";
+    class MockApp implements IApp {
+        title:string = appTitle;
+	    currentPageName:string = pageName;
+	    startPageName:string = pageName;
+	    pages:Array<IPage> = [page1, page2];
+	    injectActionService(as:IActionService) {};
+	    startAddingPages() {};
+	    CentralCallbackFunc(pageName:string, elementID?:string) {};
+    }
+    let testStateService: StateService = new StateService(new MockApp());  
+    it('getStartPageName() should return the specified string', () => {
         let startPageName: string  = testStateService.getStartPageName();
-        expect(startPageName).toEqual(checkThisValue);
+        expect(startPageName).toEqual(pageName);
+    });
+    it('getStartPageName() should return a string', () => {
+        let currentPageName: string = testStateService.getCurrentPageName();
+        expect(currentPageName).toEqual(pageName);
+    });
+    it('setCurrentPageName() should set the name of the current page', () => {
+    	let newName :string = "New Name";
+    	testStateService.setCurrentPageName(newName);
+        let currentPageName: string = testStateService.getCurrentPageName();
+        expect(currentPageName).toEqual(newName);
+    });
+    it("getCurrentPage() should return an IPage object of the current page", () => {
+    	let currentPage: IPage = testStateService.getCurrentPage();
+    	expect(currentPage.name).toEqual("page1");
     });
 });
+
+/*Previous tests for reference
+/// <reference path="../typings/jasmine/jasmine.d.ts" />
+
+describe('Tests for the FindViewModel', () => {
+    it('getKeyword() should return a string', () => {
+        let returnValue: string = (new FindViewModel()).getKeyword();
+        expect(returnValue).toEqual(jasmine.any(String));
+    });
+
+    it('getKeywordByVoice() should return a string', () => {
+        let checkThisValue: string;
+        class mockSpeech implements ISpeechService {
+            recognize(): string {
+                return checkThisValue;
+            }
+        }
+
+        let testFindViewModel = new FindViewModel();
+        let findKeywordByVoice = testFindViewModel.getKeywordByVoice(new mockSpeech());
+        expect(findKeywordByVoice).toEqual(checkThisValue);
+    });
+});
+
+describe('Test for the HistoryViewModel', () => {
+    it('getHistoryList() should return something', () => {
+        let vm = new HistoryViewModel(new StorageService());
+        let returnValue: IListItem[] = vm.getHistoryList();
+        expect(returnValue).not.toBeNull();
+    });
+});
+
+describe('Test for the RecommendViewModel', () => {
+    it('getHistoryList() should return something', () => {
+        let vm = new RecommendViewModel(new StorageService());
+        let returnValue: IListItem[] = vm.getHistoryList();
+        expect(returnValue).not.toBeNull();
+    });
+});
+*/
--- temp/spec/test.spec.js
+++ temp/spec/test.spec.js
@@ -1,27 +1,52 @@
 "use strict";
 var StateService_1 = require("../src/emulator/services/StateService");
+var _ = require('../../dist/assets/lodash.js');
 it('true is true', function () { return expect(true).toEqual(true); });
 describe('Tests for the StateService', function () {
-    it('getStartPageName() should return a string', function () {
-        var checkThisValue = "This Page";
-        var MockApp = (function () {
-            function MockApp() {
-                this.title = "";
-                this.currentPageName = checkThisValue;
-                this.startPageName = checkThisValue;
-            }
-            MockApp.prototype.injectActionService = function (as) { };
-            ;
-            MockApp.prototype.startAddingPages = function () { };
-            ;
-            MockApp.prototype.CentralCallbackFunc = function (pageName, elementID) {
-                return "";
-            };
-            return MockApp;
-        }());
-        var testStateService = new StateService_1.StateService(new MockApp());
+    var MockPage = (function () {
+        function MockPage() {
+        }
+        return MockPage;
+    }());
+    var page1 = new MockPage();
+    page1.name = "page1";
+    var page2 = new MockPage();
+    page2.name = "page2";
+    var pageName = "page1";
+    var appTitle = "Title";
+    var MockApp = (function () {
+        function MockApp() {
+            this.title = appTitle;
+            this.currentPageName = pageName;
+            this.startPageName = pageName;
+            this.pages = [page1, page2];
+        }
+        MockApp.prototype.injectActionService = function (as) { };
+        ;
+        MockApp.prototype.startAddingPages = function () { };
+        ;
+        MockApp.prototype.CentralCallbackFunc = function (pageName, elementID) { };
+        ;
+        return MockApp;
+    }());
+    var testStateService = new StateService_1.StateService(new MockApp());
+    it('getStartPageName() should return the specified string', function () {
         var startPageName = testStateService.getStartPageName();
-        expect(startPageName).toEqual(checkThisValue);
+        expect(startPageName).toEqual(pageName);
+    });
+    it('getStartPageName() should return a string', function () {
+        var currentPageName = testStateService.getCurrentPageName();
+        expect(currentPageName).toEqual(pageName);
+    });
+    it('setCurrentPageName() should set the name of the current page', function () {
+        var newName = "New Name";
+        testStateService.setCurrentPageName(newName);
+        var currentPageName = testStateService.getCurrentPageName();
+        expect(currentPageName).toEqual(newName);
+    });
+    it("getCurrentPage() should return an IPage object of the current page", function () {
+        var currentPage = testStateService.getCurrentPage();
+        expect(currentPage.name).toEqual("page1");
     });
 });
 //# sourceMappingURL=test.spec.js.map
