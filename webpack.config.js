0c184b4ae8ed044f277084a525344b794faf0e2b
Thomas Scott <emailthomasscott@gmail.com>
2016-08-19 14:03:14 +1200

    Report edit finished.
    Also removed large block comment in weback config.
    Changed title of test page.
    Changed some of the wording on the dist index html page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,7 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
-/*
-module.exports = {
-    entry: {
-        'scripts/emulator.bundle.js':'./temp/emulator/ui/emulator.js',
-        'css/index.css':'./temp/emulator/ui/css/index.css'
-    },
-    output: {
-        path: './dist/',
-        filename: '[name]'
-    },
-    resolve: {
-        extension:['','.js']
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("css-loader?sourceMap")
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('css/index.css',{allChunks:false}),
-        new webpack.optimize.UglifyJsPlugin()
-    ]
-}
-*/
+
 //Below is a configuration for transpile ts directly to js
 module.exports = {
     context: __dirname+"/src",
