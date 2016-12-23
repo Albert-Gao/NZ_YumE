d8ec5e5cd31693cd01f17077488f38572e7e12d5
Albert Gao <albertgaohy@gmail.com>
2016-12-23 14:14:00 +1300

    delete every files that not relate to the demo

--- temp/emulator/models/serviceModels/ITemplatingService.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=ITemplatingService.js.map
--- temp/spec/test2.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-jasmine.getFixtures().fixturesPath = "../spec/";
-describe('Tests for ActionService', function () {
-    it('goPage() should call goPage from SystemService', function () {
-        expect(true).toBe(true);
-    });
-});
-//# sourceMappingURL=test2.spec.js.map
